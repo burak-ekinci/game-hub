9b659344ca03a46e8fd43dddaf0c1d63a8181b99
Burak Ekinci <90180594+burak-ekinci@users.noreply.github.com>
2024-05-13 17:23:30 +0300

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params : {
-        key: "9afa56b5824e402e8b82a57ddf490792",
+        key: "************",
     }
 })
